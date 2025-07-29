837450136694095629257033794a47149d138964
Elizabeth Craig <elcraig@microsoft.com>
2025-07-29 13:57:27 -0700

    fix type issues

--- src/collisions-resolver.ts
+++ src/collisions-resolver.ts
@@ -42,7 +42,7 @@ export class CollisionsResolver {
 	/**
 	 * Adds (or "registers") a top-level {@link identifier} (which takes a top-level scope name to use).
 	 */
-	public addTopLevelIdentifier(identifier: ts.Identifier | ts.DefaultKeyword): string {
+	public addTopLevelIdentifier(identifier: ts.Identifier | ts.DefaultKeyword | ts.ModuleExportName): string {
 		const symbol = getDeclarationNameSymbol(identifier, this.typeChecker);
 		if (!symbol) {
 			throw new Error(
